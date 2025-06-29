35c5c0bb276d11da8f4f687fa6986ed6b904b4b4
HKRK8569 <hkrk3021@gmail.com>
2025-06-30 00:15:06 +0900

    カメラコントロール機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <div className="h-screen w-screen">
-      <Canvas>
+      <Canvas camera={{ position: [-3, 0.5, 3] }}>
         <mesh>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh>
+        <CameraControls makeDefault />
       </Canvas>
     </div>
   );
